4e189a1a2906a97c23a9f1536622d18eab02f34b
Enda Quigley <endaquigley@gmail.com>
2018-01-17 20:46:41 +0000

    create thumbnail component

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,34 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import styled from 'styled-components';
-import Modal from './Modal.js'
+import Thumbnail from './Thumbnail.js';
+import Modal from './Modal.js';
 
 const Wrapper = styled.div`
-  margin: 20px 0;
+  margin: 15px;
   display: grid;
-  grid-gap: 10px;
+  grid-gap: 15px;
   grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
-`;
-
-const Thumbnail = styled.div`
-  position: relative;
-  cursor: pointer;
-  overflow: hidden;
-  transform: scale(1);
-  will-change: transform;
-  transition: transform 0.3s ease;
-  padding-bottom: 100%;
-  &:hover {
-    transform: scale(0.9);
-  }
-`;
-
-const Preview = styled.video`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
 `;
 
 class Gallery extends Component {
@@ -51,17 +30,11 @@ class Gallery extends Component {
           {
             this.props.data.map((media) => {
               return (
-                <div key={ media.id }>
-                  <Thumbnail>
-                    <Preview
-                      loop
-                      muted
-                      autoPlay
-                      src={ media.images.preview.mp4 }
-                      onClick={ () => this.setSelected(media) }
-                    />
-                  </Thumbnail>
-                </div>
+                <Thumbnail
+                  key={ media.id }
+                  source={ media.images.preview.mp4 }
+                  handleClick={ () => this.setSelected(media) }
+                />
               )
             })
           }
---
+++ src/Thumbnail.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  position: relative;
+  cursor: pointer;
+  overflow: hidden;
+  transform: scale(1);
+  will-change: transform;
+  transition: transform 0.3s ease;
+  background-color: rgba(0, 0, 0, 0.25);
+  border: 1px solid rgba(0, 0, 0, 0.75);
+  padding-bottom: 100%;
+  &:hover {
+    transform: scale(0.9);
+  }
+`;
+
+const Preview = styled.video`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const Thumbnail = ({ source, handleClick }) => {
+  return (
+    <Wrapper>
+      <Preview
+        loop
+        muted
+        autoPlay
+        src={ source }
+        onClick={ () => handleClick() }
+      />
+    </Wrapper>
+  )
+};
+
+export default Thumbnail;
