de44469fb91a9dc97502b3401fba8e1d37b29ba0
Enda Quigley <endaquigley@gmail.com>
2018-01-18 01:11:31 +0000

    fix typo

--- src/Thumbnail.js
+++ src/Thumbnail.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const GridChild = styled.div`
-  /* perserve aspect ratio in Firefox */
+  /* preserve aspect ratio in Firefox */
 `;
 
 const Square = styled.div`
