3b8fd1ac9d52135b96d2805f608d6697a03100d6
Enda Quigley <endaquigley@gmail.com>
2018-12-20 13:34:35 +0000

    use implicit returns for actions

--- src/actions.js
+++ src/actions.js
@@ -1,32 +1,22 @@
-export const updateQuery = query => {
-  return {
-    type: "UPDATE_QUERY",
-    query: query
-  };
-};
+export const updateQuery = query => ({
+  type: "UPDATE_QUERY",
+  query: query
+});
 
-export const updateData = data => {
-  return {
-    type: "UPDATE_DATA",
-    data: data
-  };
-};
+export const updateData = data => ({
+  type: "UPDATE_DATA",
+  data: data
+});
 
-export const updateSelected = selected => {
-  return {
-    type: "UPDATE_SELECTED",
-    selected: selected
-  };
-};
+export const updateSelected = selected => ({
+  type: "UPDATE_SELECTED",
+  selected: selected
+});
 
-export const nextPage = () => {
-  return {
-    type: "NEXT_PAGE"
-  };
-};
+export const nextPage = () => ({
+  type: "NEXT_PAGE"
+});
 
-export const prevPage = () => {
-  return {
-    type: "PREV_PAGE"
-  };
-};
+export const prevPage = () => ({
+  type: "PREV_PAGE"
+});
