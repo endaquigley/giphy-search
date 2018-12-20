a509db0488b9f8455b21d5b47ad39ea2df1ed955
Enda Quigley <endaquigley@gmail.com>
2018-12-20 13:37:52 +0000

    create reducer file

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,49 +1,5 @@
 import { useReducer } from "react";
-
-const initialState = {
-  page: 0,
-  data: [],
-  query: "dog",
-  selected: undefined
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "UPDATE_DATA":
-      return {
-        ...state,
-        data: action.data
-      };
-
-    case "UPDATE_SELECTED":
-      return {
-        ...state,
-        selected: action.selected
-      };
-
-    case "UPDATE_QUERY":
-      return {
-        ...state,
-        query: action.query,
-        page: 0
-      };
-
-    case "NEXT_PAGE":
-      return {
-        ...state,
-        page: state.page + 1
-      };
-
-    case "PREV_PAGE":
-      return {
-        ...state,
-        page: state.page - 1
-      };
-
-    default:
-      return state;
-  }
-};
+import { reducer, initialState } from "../reducer";
 
 const useStore = () => {
   return useReducer(reducer, initialState);
